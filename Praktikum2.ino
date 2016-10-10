ef31b469017963eb7ac48a483f77e6ffd836bf50
BayuAjiNugraha <bayuajingrh@yahoo.com>
2016-10-10 11:00:06 +0700

    Add files via upload

---
+++ Praktikum2.ino
@@ -0,0 +1,17 @@
+void setup() {
+  Serial.begin(115200);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  Serial.print(0xF0);
+  Serial.print(0x01);
+  Serial.print(0x02);
+  Serial.print(0x03);
+  Serial.print(0x04);
+  delay(2000);
+  
+  // put your main code here, to run repeatedly:
+
+}
